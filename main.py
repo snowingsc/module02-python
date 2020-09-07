86f8c0cd1fa7cd484edfa70bc3afe2c98b3f04c1
Yanling Wang <wangyl77@gmail.com>
2020-09-07 14:14:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+weight = 90
+height = 56
+BMI = 703*weight/(height**2)
+print(f"BMI is {BMI}")
+if BMI < 18.5:
+  print("underweight")
+elif BMI < 25.0:
+  print("normal")
+elif BMI < 30.0:
+  print("overweight")
+else:
+  print("obese")
+
+unit = input("Enter unit: ")
+if unit == "F" or unit == "f":
+  print("I am here")
